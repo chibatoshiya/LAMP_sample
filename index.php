b8fda1a3813004e8f10d60561abc465aed43b155
chibatoshiya <t.chiba.sys19@morijyobi.ac.jp>
2020-06-18 12:44:29 +0900

    index

--- index.php
+++ index.php
@@ -6,28 +6,28 @@ try {
     $dbh = new PDO($dsn, $user, $password);
 ​
     $sql = "select * from user";
-    $result = $dbh ->query($sql);
-​
-    echo "接続成功\n";
+    $result = $dbh->query($sql);
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     exit();
 }
 ?>
-
+​
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
+​
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LAMP SAMPLE PAGE</title>
 </head>
+​
 <body>
-    <div class = "container">
-        <?php foreach( $result as $value ){
+    <div class="container">
+        <?php foreach ($result as $value) {
             echo "<h1>$value[id] $value[name] $value[age]</h1>";
-        }
-        ?>
+        } ?>
     </div>
 </body>
+​
 </html>
